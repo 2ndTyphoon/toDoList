81b20ee5d6ecc34365a39b873d6ad3e202ff1f39
2ndTyphoon <akasshaframe@gmail.com>
2024-10-24 02:31:55 +0300

    Release 1.0 ^_^

--- main.py
+++ main.py
@@ -6,30 +6,29 @@ def add_task(file_path):
 
 
 def remove_task(file_path):
-    #check_tasks(file_path)
-
     with open(file_path, "r") as file:
         lines = file.readlines()
 
-    print(len(lines))
-
-    if len(lines) == 0:
-        print(f"Пока что тут пусто..." '\n'
-              f"Давайте что-нибудь добавим!")
-    else:
-        remove_line = int(input(f"#Какую задачу удалить?: "))
-        if remove_line < 1 or remove_line > len(lines) + 1:
-            print(f"#Неверная задача для удаления, попробуйте ещё раз")
-        return
-
-        del lines[remove_line - 1]
-
-        with open(file_path, "w") as file:
-            file.writelines(lines)
-
-        print(f"#Задача удалена")
-
+    if not lines:
         check_tasks(file_path)
+    else:
+        check_tasks(file_path)
+        remove_line = int(input(f"#Какую задачу удалить?: "))
+
+        if remove_line < 1 or remove_line > len(lines):
+            print(f"#Неверная задача для удаления, попробуйте ещё раз")
+            return
+        else:
+            del lines[remove_line - 1]
+            with open(file_path, "w") as file:
+                file.writelines(lines)
+            print(f"#Задача удалена")
+
+        if not lines:
+            print(f"#Теперь тут пусто..." '\n'
+                  f"#Давайте что-нибудь добавим!")
+        else:
+            check_tasks(file_path)
 
 
 def check_tasks(file_path):
@@ -37,8 +36,8 @@ def check_tasks(file_path):
         lines = file.readlines()
 
         if not lines:
-            print(f"Пока что тут пусто..." '\n'
-              f"Давайте что-нибудь добавим!")
+            print(f"#Пока что тут пусто..." '\n'
+              f"#Давайте что-нибудь добавим!")
         else:
             with open(file_path, "r") as file:
                 whatToDo = file.read()
@@ -54,13 +53,22 @@ print(f"#Для перехода к задачи, введите её номер
       f"#3. Удалить существующую задачу" '\n'
       f"#4. Выход" '\n')
 
-user_input = ""
+user_input = None
 
 while user_input != 4:
-    user_input = int(input("#Что будем делать?: "))
+    user_input = input("#Что будем делать?: ")
+
+    try:
+        user_input = int(user_input)
+    except ValueError:
+        print(f"Введена не цифра, пожалуйста, попробуйте снова")
+        continue
+
     if user_input == 1:
         check_tasks(file_path)
     elif user_input == 2:
         add_task(file_path)
     elif user_input == 3:
         remove_task(file_path)
+    else:
+        print(f"Такого выбора нет, пожалуйста, выберите задачу от 1 до 4")
